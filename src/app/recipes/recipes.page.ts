82641eeae596e931ebd22958b92fb72ab08060a5
Trevor Sawler <trevor.sawler@gmail.com>
2020-07-18 19:52:06 -0300

    angular routing

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe} from "./recipe.model";
+
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
+
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: 'https://www.dessertfortwo.com/wp-content/uploads/2014/12/Schnitzel-2.jpg',
+      ingredients: ['French Fries', 'Pork Meat', 'Salad']
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: 'https://pinchofyum.com/wp-content/uploads/2013/06/spaghetti-9-819x1024.jpg',
+      ingredients: ['Spaghetti', 'Meat', 'Tomatoes']
+    }
+  ]
 
   constructor() { }
 
