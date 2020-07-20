2162b167b396a34782cad122fd8124b36d773bd6
Trevor Sawler <trevor.sawler@gmail.com>
2020-07-20 12:25:56 -0300

    Delete hosts

--- src/app/servers/host/host.page.ts
+++ src/app/servers/host/host.page.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ServerService} from '../server.service';
 import {ServerModel} from '../server.model';
+import {AlertController} from '@ionic/angular';
 
 @Component({
     selector: 'app-host',
@@ -15,7 +16,8 @@ export class HostPage implements OnInit {
     constructor(
         private activatedRoute: ActivatedRoute,
         private hostService: ServerService,
-        private router: Router) {
+        private router: Router,
+        private alertCtrl: AlertController) {
     }
 
     ngOnInit() {
@@ -30,8 +32,26 @@ export class HostPage implements OnInit {
     }
 
     onDeleteServer() {
-        this.hostService.deleteServer(this.loadedServer.id);
-        this.router.navigate(['/servers']);
+        this.alertCtrl.create({
+            header: 'Are you sure?',
+            message: 'Delete this host?',
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                },
+                {
+                    text: 'Delete',
+                    handler: () => {
+                        this.hostService.deleteServer(this.loadedServer.id);
+                        this.router.navigate(['/servers']);
+                    }
+                }
+            ]
+        }).then(alertEl => {
+            alertEl.present();
+        });
+
     }
 
 }
