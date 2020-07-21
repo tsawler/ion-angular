0f3b1f1797ccb3b8083e454d81744730a38467aa
Trevor Sawler <trevor.sawler@gmail.com>
2020-07-21 14:12:14 -0300

    Navigation

--- src/app/problems/problem-service.service.ts
+++ src/app/problems/problem-service.service.ts
@@ -9,7 +9,15 @@ export class ProblemServiceService {
     private problems: ProblemModel[] = [
         {
             id: '1',
+            type: 'warning',
             serviceName: 'https',
+            hostName: 'GoBlender',
+            problemStart: '2020-07-21 09:00:01 AM',
+        },
+        {
+            id: '2',
+            type: 'problem',
+            serviceName: 'postgres',
             hostName: 'GoBlender',
             problemStart: '2020-07-21 09:00:01 AM',
         },
--- src/app/problems/problem.model.ts
+++ src/app/problems/problem.model.ts
@@ -2,6 +2,7 @@ import {ProblemServiceService} from './problem-service.service';
 
 export interface ProblemModel {
     id: string;
+    type: string;
     serviceName: string;
     hostName: string;
     problemStart: string;
